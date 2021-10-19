c8518f5bb5cf98450c910efaac7e0b90144ee8a8
shadow <shadowngoz@gmail.com>
2021-10-19 13:33:23 -0500

    Autogenerated tree updates.
    merged:
    
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      kit-fixups: 88a533af18715d0c1b57754ce2c9c4fdd7dec6e1

---
+++ sys-apps/hwinfo/autogen.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from bs4 import BeautifulSoup
+
+async def generate(hub, **pkginfo):
+	html_data = await hub.pkgtools.fetch.get_page("https://www.github.com/openSUSE/hwinfo/releases")
+	soup = BeautifulSoup(html_data, "html.parser")
+	links = soup.find_all("a")
+	version = None
+
+	for link in links:
+		href = link.get("href")
+		if href and "tag" in href:
+			parts = href.split("/")
+			version = parts[-1]
+
+			try:
+				list(map(int, version.split(".")))
+				break
+
+			except ValueError:
+				continue
+
+	if version:
+		url = f"https://github.com/openSUSE/hwinfo/archive/{version}/hwinfo-{version}.tar.gz"
+		final_name = f"hwinfo-{version}.tar.gz"
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			version=version,
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
+		)
+
+		ebuild.push()
+
+
+# vim: ts=4 sw=4 noet
